a6f1f3a8bcaeea3afaa8ac45f495230176c07ee4
Rodrigo Zimmerle <rodrigozimmerle@outlook.com>
2023-02-03 17:43:38 -0300

    ready_to_deploy

--- infraestructure/s3_files.tf
+++ infraestructure/s3_files.tf
@@ -2,5 +2,5 @@ resource "aws_s3_object" "codigo-glue-job" {
   bucket = "${var.bucket_name}"
   key    = "/scripts/pyspark_glue_job_from_tf.py" # Diretório onde será gravado o arquivo
   source = "pyspark_glue_job.py"          # Arquivo com o job que vai subir
-  etag   = filemd5("../pyspark_glue_job.py") # Define qual é o objeto de parâmetro
+  etag   = md5("../pyspark_glue_job.py") # Define qual é o objeto de parâmetro
 }
